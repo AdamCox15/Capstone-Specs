a6cf1ad14fed1b129c45b7b794c37b10322df43d
Adam Cox <adamcox.dev@gmail.com>
2022-11-22 13:38:47 -0700

    Starting server buildout

--- seed_database.py
+++ seed_database.py
@@ -14,5 +14,15 @@ os.system("createdb hockey_shop")
 model.connect_to_db(server.app)
 model.db.create_all()
 
-with open('data/hockey_shop.json') as f:
+with open('data/products.json') as f:
     hockey_data = json.loads(f.read())
+
+hockey_in_db = []
+for hockey in hockey_data:
+    name, product_path, price, brand, category = (
+        hockey["name"], hockey["product_path"], hockey["price"], hockey["brand"], hockey["category"])
+    db_hockey = crud.create_product(name, product_path, price, brand, category)
+    hockey_in_db.append(db_hockey)
+    print(hockey_in_db)
+model.db.session.add_all(hockey_in_db)
+model.db.session.commit()
