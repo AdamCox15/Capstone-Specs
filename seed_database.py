28a4bfdceaf41e1e9946de43c03a71002c95450e
Adam Cox <adamcox.dev@gmail.com>
2022-11-22 13:18:10 -0700

    seeding database

--- seed_database.py
+++ seed_database.py
@@ -6,3 +6,13 @@ from datetime import datetime
 import crud
 import model
 import server
+
+
+os.system("dropdb hockey_shop")
+os.system("createdb hockey_shop")
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/hockey_shop.json') as f:
+    hockey_data = json.loads(f.read())
